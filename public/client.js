966a5ad0d21159ca29a29b4be8d615da29517dca
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-05-18 08:34:03 +0400

    Listen on incoming event and show connected users.

--- public/client.js
+++ public/client.js
@@ -1,22 +1,53 @@
+// @ts-nocheck
 let ws;
-let elOnlineUsers = document.querySelector('#onlineUserCount');
-let btnNickname = document.querySelector('#sendNickName');
-let inputNickname = document.querySelector('#inputNickname');
+let chatUsersCtr = document.querySelector("#chatUsers");
 
-btnNickname.onclick = (ev) => {
-  const data = {event: 'join', nickname: inputNickname.value};
-  ws.send(JSON.stringify(data));
-  inputNickname.value = '';
+window.addEventListener("DOMContentLoaded", () => {
+  ws = new WebSocket(`ws://localhost:3000/ws`);
+  ws.addEventListener("open", onConnectionOpen);
+  ws.addEventListener("message", onMessageReceive);
+});
+
+function onConnectionOpen() {
+  const queryParams = getQueryParams();
+  console.log(queryParams);
+  if (!queryParams.group || !queryParams.name) {
+    window.location.href = 'chat.html';
+  }
+  emitEvent(
+    { event: "join", groupName: queryParams.group, name: queryParams.name },
+    ws,
+  );
 }
 
-const onMessageReceive = (ev) => {
-  console.log("Event received ", ev);
-  const data = JSON.parse(ev.data);
-  if (data.event === 'onlineUsers'){
-    elOnlineUsers.innerHTML = data.count;
+function onMessageReceive(event) {
+  console.log(event);
+  const data = JSON.parse(event.data);
+  switch (data.event) {
+    case "users":
+      const users = data.data;
+      chatUsersCtr.innerHTML = '';
+      users.forEach((u) => {
+        const userEl = document.createElement("div");
+        userEl.className = "chat-user";
+        userEl.innerHTML = u.name;
+        chatUsersCtr.appendChild(userEl);
+      });
+      break;
   }
 }
-window.addEventListener('DOMContentLoaded', () => {
-  ws = new WebSocket(`ws://localhost:3000/ws`);
-  ws.addEventListener('message', onMessageReceive);
-})
+
+function getQueryParams() {
+  const queryStr = window.location.search.substring(1);
+  const pairs = queryStr.split("&");
+  const obj = {};
+  for (let p of pairs) {
+    const parts = p.split("=");
+    obj[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+  }
+  return obj;
+}
+
+function emitEvent(obj, ws) {
+  ws.send(JSON.stringify(obj));
+}
