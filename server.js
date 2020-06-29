a930e08b7d71a3193c4b2e9a5f4e0c388bca1ce9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-29 21:39:27 +0400

    Add to serve static files from Deno

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 import { listenAndServe } from "https://deno.land/std@0.58.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.58.0/http/file_server.ts";
 import { acceptWebSocket, acceptable } from "https://deno.land/std@0.58.0/ws/mod.ts";
 import chat from "./chat.js";
 
+async function fileExists(path) {
+  try {
+    const stats = await Deno.lstat(path);
+    return stats && stats.isFile;
+  } catch(e) {
+    if (e && e instanceof Deno.errors.NotFound) {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
 listenAndServe({ port: 3000 }, async (req) => {
+
+  const position = req.url.indexOf('?');
+
+  let url = req.url;
+  if (position > -1) {
+    url = req.url.substring(0, position);
+  }
+  const path = `${Deno.cwd()}/public${url}`; // /index.html
+  if (await fileExists(path)) {
+    const content = await serveFile(req, path);
+    req.respond(content);
+    return; 
+  }
+
   if (req.method === "GET" && req.url === "/ws") {
     if (acceptable(req)) {
       acceptWebSocket({
